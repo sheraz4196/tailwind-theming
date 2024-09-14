e9599c4c72b16863a56b3b194414fdd492a39925
hassnainzia121 <hassnainzia.dev121@gmail.com>
2024-09-14 15:11:11 +0500

    first card theme complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ["./public/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        skin: {
+          fill: "var(--color-fill)",
+          base: "var(--color-text-base)",
+          muted: "var(--color-text-muted)",
+          inverted: "var(--color-text-inverted)",
+        },
+      },
+      backgroundColor: {
+        skin: {
+          fill: "var(--color-fill)",
+          "button-accent": "var(--color-button-accent)",
+          "button-accent-hover": "var(--color-button-accent-hover)",
+          "button-muted": "var(--color-button-muted)",
+          "button-muted-hover": "var(--color-button-muted-hover)",
+        },
+      },
+      gradientColorStops: {
+        skin: {
+          hue: "var(--color-fill)",
+          saturation: "var(--color-fill-two)",
+        },
+      },
+    },
   },
   plugins: [],
 };
