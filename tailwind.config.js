0fc4e6a2bca52b413825537d5d9a75e992c40636
hassnainzia121 <hassnainzia.dev121@gmail.com>
2024-09-14 16:21:12 +0500

    theme second complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,6 @@ module.exports = {
           fill: "var(--color-fill)",
           base: "var(--color-text-base)",
           muted: "var(--color-text-muted)",
-          inverted: "var(--color-text-inverted)",
         },
       },
       backgroundColor: {
@@ -16,8 +15,8 @@ module.exports = {
           fill: "var(--color-fill)",
           "button-accent": "var(--color-button-accent)",
           "button-accent-hover": "var(--color-button-accent-hover)",
-          "button-muted": "var(--color-button-muted)",
-          "button-muted-hover": "var(--color-button-muted-hover)",
+          "button-muted-hover": "var(--color-button-muted)",
+          "button-muted": "var(--color-button-muted-hover)",
         },
       },
       gradientColorStops: {
