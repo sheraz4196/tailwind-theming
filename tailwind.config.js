e44cb2fd04b3aeade13957ff04f402cf4ba8c10e
hassnainzia121 <hassnainzia.dev121@gmail.com>
2024-09-16 13:53:52 +0500

    create black theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
   content: ["./public/*.html"],
   theme: {
     extend: {
+      borderColor: {
+        primary: withopacity("--border-color"),
+      },
       textColor: {
         skin: {
           fill: withopacity("--color-fill"),
@@ -21,11 +24,12 @@ module.exports = {
       },
       backgroundColor: {
         skin: {
-          fill: "var(--color-fill)",
+          fill: withopacity("--color-fill"),
           "button-accent": withopacity("--color-button-accent"),
           "button-accent-hover": withopacity("--color-button-accent-hover"),
           "button-muted-hover": withopacity("--color-button-muted-hover"),
           "button-muted": withopacity("--color-button-muted"),
+          camera: withopacity("--color-camera-button"),
         },
       },
       gradientColorStops: {
